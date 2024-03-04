e5f00a937bae9ed04dbb6c02c6fe5d41091c5c0e
gratus richard a <gratusri@gmail.com>
2024-03-04 19:00:21 +0530

    comit

--- app.py
+++ app.py
@@ -1,24 +1,34 @@
 import os
 import random
-
+from PIL import Image
 path = '/home/gratus/Downloads/dataset'
+
+outputdir = '/media/gratus/HDD/TRS/paddlecustom/maindataset'
+
+
 dirs = os.listdir(path)
 
-with open('/media/gratus/HDD/TRS/paddlecustom/label.txt', 'a') as file:
+
+textfile = '/media/gratus/HDD/TRS/paddlecustom/label.txt'
+
+with open(textfile, 'a') as file:
     for dir in dirs:
         picdirpath = os.path.join(path, dir)
         pics = os.listdir(picdirpath)
+        i =0
         for pic in pics:
             pic_path = os.path.join(picdirpath, pic)
-            file.write(pic_path + '\t' + dir + '\n')
 
-# Read the lines from the file
-with open('/media/gratus/HDD/TRS/paddlecustom/label.txt', 'r') as file:
+            image = Image.open(pic_path)
+            savepath  = os.path.join(outputdir,f'{i}'+dir+".jpeg")
+            image.save(savepath,format="JPEG")
+            file.write(savepath + '\t' + dir + '\n')
+            i+=1
+
+with open(textfile, 'r') as file:
     lines = file.readlines()
 
-# Shuffle the lines
 random.shuffle(lines)
 
-# Write the shuffled lines back to the file
-with open('/media/gratus/HDD/TRS/paddlecustom/label.txt', 'w') as file:
+with open(textfile, 'w') as file:
     file.writelines(lines)
